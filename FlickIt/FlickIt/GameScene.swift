23f08ed6b2611e7f0301fea668b65c465fc7a34c
Ashwin <ashyv97@gmail.com>
2016-02-20 10:16:29 -0800

    First prototype of home screen

--- FlickIt/FlickIt/GameScene.swift
+++ FlickIt/FlickIt/GameScene.swift
@@ -26,8 +26,84 @@ class GameScene: SKScene {
     let kMinSpeed=CGFloat(100)
     let kMaxSpeed=CGFloat(500)
     
+    var bgImage = SKSpriteNode(imageNamed: "neon_circle.jpg");
+    var startSquare = SKSpriteNode(imageNamed: "start_square.jpg");
+    var launchSquare = SKSpriteNode(imageNamed: "launch_square.jpg");
+    var rulesCircle = SKSpriteNode(imageNamed: "rules_circle.jpg");
+    var launchCircle = SKSpriteNode(imageNamed: "launch_circle.jpg");
+    
+    func createHomeScreen(){
+        // Sets the neon circle image to fill the entire screen
+        bgImage.size = CGSize(width: self.size.width/2, height: self.size.height);
+        bgImage.position = CGPointMake(self.size.width/2, self.size.height/2);
+        
+        // Put the start square bin at the top center and rules circle bin at the bottom center
+        startSquare.position = CGPointMake(self.frame.width/2, self.frame.height * 9 / 10);
+        rulesCircle.position = CGPointMake(self.frame.width/2, self.frame.height / 10);
+        
+        // Start the launch square and circle in the very middle of the screen
+        launchSquare.position = CGPointMake(self.size.width/2, self.size.height/2);
+        launchCircle.position = CGPointMake(self.size.width/2, self.size.height/2);
+        
+        // Set names for the launcher so that we can check what node is touched in the touchesEnded method
+        launchSquare.name = "launch square";
+        launchCircle.name = "launch circle";
+        
+        /** Add Physics Stuff for square launcher */
+        launchSquare.userInteractionEnabled=false
+        launchSquare.physicsBody=SKPhysicsBody(rectangleOfSize: CGSize(width: launchSquare.size.width, height: launchSquare.size.height));
+        launchSquare.physicsBody?.dynamic=true
+        launchSquare.physicsBody?.affectedByGravity=false
+        
+        /** Add Physics Stuff for circle launcher */
+        launchCircle.userInteractionEnabled=false
+        launchCircle.physicsBody=SKPhysicsBody(circleOfRadius: launchCircle.frame.height/2)
+        launchCircle.physicsBody?.dynamic=true
+        launchCircle.physicsBody?.affectedByGravity=false
+        
+        // Create and add title to home screen CURRENTLY ONLY WORKS IF YOU DO title.center = CGPointMake(160, 284)
+        var title = UILabel(frame: CGRectMake(0, 0, 200, 21))
+        title.center = CGPointMake(self.frame.width/2, self.frame.height/2)
+        title.textAlignment = NSTextAlignment.Center
+        
+        title.text = "Flick-It"
+        title.font = UIFont(name: "Futura", size: 30)
+        title.textColor = UIColor.blueColor()
+        self.view?.addSubview(title)
+        
+        // Add all the elements to the screen
+        self.addChild(bgImage);
+        self.addChild(startSquare);
+        self.addChild(launchSquare);
+        
+        self.addChild(rulesCircle);
+        self.addChild(launchCircle);
+
+        // Have square first shoot up quickly, then slower and slower.
+        launchSquare.runAction(SKAction.sequence([
+            SKAction.moveBy(CGVectorMake(0, self.frame.height / 15), duration: 1),
+            SKAction.moveBy(CGVectorMake(0, self.frame.height / 15), duration: 15),
+            SKAction.moveBy(CGVectorMake(0, self.frame.height / 20), duration: 50)
+            ])
+        )
+        // Have circle first shoot down quickly, then slower and slower.
+        launchCircle.runAction(SKAction.sequence([
+            SKAction.moveBy(CGVectorMake(0, -self.frame.height / 15), duration: 1),
+            SKAction.moveBy(CGVectorMake(0, -self.frame.height / 15), duration: 15),
+            SKAction.moveBy(CGVectorMake(0, -self.frame.height / 20), duration: 50)
+            ])
+        )
+        
+        
+        
+    }
+    
     /** Creates scene by setting position and defining physics */
     func createScene(){
+        
+        /** Puts in background */
+        self.view?.backgroundColor = UIColor.blackColor()
+        
         /** Puts in the title */
         let title = UILabel(frame: CGRectMake(self.frame.width/9, self.frame.height/8, self.frame.width/2, self.frame.height/8))
         
@@ -60,8 +136,7 @@ class GameScene: SKScene {
 
         
         
-        /** Puts in background */
-        self.view?.backgroundColor = UIColor.blackColor()
+        
 
         
         /** Giving the objects pictures */
@@ -97,7 +172,7 @@ class GameScene: SKScene {
     }
     
     override func didMoveToView(view: SKView) {
-        createScene()
+        createHomeScreen()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -123,11 +198,36 @@ class GameScene: SKScene {
             let touchedNode=self.nodeAtPoint(start)
             //make it move
             touchedNode.physicsBody?.velocity=CGVectorMake(0.0, 0.0)
-            touchedNode.physicsBody?.applyImpulse(CGVectorMake(100*dx, 100*dy))
+            if (touchedNode.name == "launch square" && dy > 0){
+                touchedNode.physicsBody?.applyImpulse(CGVectorMake(0, 100*dy))
+            }
+            if (touchedNode.name == "launch circle" && dy < 0){
+                touchedNode.physicsBody?.applyImpulse(CGVectorMake(0, 100*dy))
+            }
+            //touchedNode.physicsBody?.applyImpulse(CGVectorMake(100*dx, 100*dy))
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        if (launchSquare.position.y >= startSquare.position.y){
+            // call method to start game
+            // for now just remove all the elements to show something has happened
+            self.removeAllChildren();
+            self.addChild(bgImage);
+        }
+        
+        if (launchCircle.position.y <= rulesCircle.position.y){
+            // call method to start game
+            // for now just remove all the elements to show something has happened
+            self.removeAllChildren();
+            self.addChild(bgImage);
+        }
+    }
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time( DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
+        
     }
 }
