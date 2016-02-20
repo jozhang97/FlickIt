973bc7de46223dfaa85a4f8d46cb127b74a1da8f
jozhang97 <jozhang@berkeley.edu>
2016-02-20 15:40:28 -0800

    Four different shapes will randomly show up instead of one general one

--- FlickIt/FlickIt/SpawnShape.swift
+++ FlickIt/FlickIt/SpawnShape.swift
@@ -8,46 +8,52 @@
 
 import SpriteKit
 import Foundation
-// next step: randomize the shape image/name to four shapes
 class SpawnShape: SKScene
 {
-    let shapeCounter = [Int]()
+
+    let shapes = ["blue_triangle", "red_square", "green_triangle","yellow_square"]
+    var shapeCounter = [0,0,0,0]
     let delayTime = 2.0 // time between spawns
     static let range = 100.0 //range of X velocities
     let X_VELOCITY_RANGE = CGFloat(range)
     let Y_VELOCITY_RANGE = CGFloat(1.5*range)
     let LOWERBOUND = CGFloat(40.0) // smallest velocity possible
+    
     func spawnShape() {
-    let shape = SKSpriteNode(imageNamed: "Spaceship")
-    shape.name = "Spaceship"
-    shape.setScale(0.1)
-    shape.physicsBody?.categoryBitMask = PhysicsCategory.Shape
-    shape.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2)
-    shape.zPosition = 5
-    shape.physicsBody = SKPhysicsBody(circleOfRadius: self.frame.height/2)
-    shape.physicsBody?.affectedByGravity = false
-    shape.physicsBody?.dynamic = true
-    
-    /** ASHWIN, CHANGE 0 TO BIN WHEN WORKING ON COLLISION */
-    shape.physicsBody?.collisionBitMask = 0 // PhysicsCategory.Bin
-    shape.physicsBody?.contactTestBitMask = 0 //PhysicsCategory.Bin
-    
-    // Randomizing velocity vectors
-    var dx = CGFloat(Float(arc4random())/0xFFFFFFFF)
-    var dy = CGFloat(Float(arc4random())/0xFFFFFFFF)
-    while (abs(dx) < LOWERBOUND && abs(dy) < LOWERBOUND)
-    {
-        dx = CGFloat(Float(arc4random())/0xFFFFFFFF)
-        dx = X_VELOCITY_RANGE*dx - X_VELOCITY_RANGE/2
-        dy = CGFloat(Float(arc4random())/0xFFFFFFFF)
-        dy = Y_VELOCITY_RANGE*dy - Y_VELOCITY_RANGE/2
-    }
-    shape.physicsBody?.velocity = CGVectorMake(dx, dy)
-    self.addChild(shape)
+        let shapePicker = (Int) (arc4random()*4 + 1)
+        let shape = SKSpriteNode(imageNamed: shapes[shapePicker])
+        shape.name = shapes[shapePicker]
+        shapeCounter[shapePicker] += 1
+        shape.setScale(0.5)
+        shape.physicsBody?.categoryBitMask = PhysicsCategory.Shape
+        shape.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2)
+        shape.zPosition = 5 // assures shape shows up over other stuff
+        shape.physicsBody = SKPhysicsBody(circleOfRadius: self.frame.height/2) //can generalize later
+        shape.physicsBody?.affectedByGravity = false
+        shape.physicsBody?.dynamic = true
+        
+        /** ASHWIN, CHANGE 0 TO BIN WHEN WORKING ON COLLISION */
+        shape.physicsBody?.collisionBitMask = 0 // PhysicsCategory.Bin
+        shape.physicsBody?.contactTestBitMask = 0 //PhysicsCategory.Bin
+        
+        // Randomizing velocity vectors
+        var dx = CGFloat(Float(arc4random())/0xFFFFFFFF)
+        var dy = CGFloat(Float(arc4random())/0xFFFFFFFF)
+        while (abs(dx) < LOWERBOUND && abs(dy) < LOWERBOUND)
+        {
+            dx = CGFloat(Float(arc4random())/0xFFFFFFFF)
+            dx = X_VELOCITY_RANGE*dx - X_VELOCITY_RANGE/2
+            dy = CGFloat(Float(arc4random())/0xFFFFFFFF)
+            dy = Y_VELOCITY_RANGE*dy - Y_VELOCITY_RANGE/2
+        }
+        shape.physicsBody?.velocity = CGVectorMake(dx, dy)
+        self.addChild(shape)
+        print("in here")
     }
 
     func startSpawning()
     {
+        print("in here")
         let spawn = SKAction.runBlock({
             () in
             self.spawnShape()
