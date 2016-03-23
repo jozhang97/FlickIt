fe6948cdaab28225184613575b06666336afec13
Abhishek Mangla <amangla@berkeley.edu>
2016-03-22 20:03:00 -0700

    home page basic stuff done

--- FlickIt/FlickIt/GameScene.swift
+++ FlickIt/FlickIt/GameScene.swift
@@ -13,12 +13,11 @@ struct PhysicsCategory {
     static let Shape : UInt32 = 0x1 << 2
 }
 
+func goToAbout() -> Void {
+    print("About:")
+}
+
 class GameScene: SKScene {
-    var startBin = SKSpriteNode()
-    var rulesBin = SKSpriteNode()
-    var aboutBin = SKSpriteNode()
-    var triangle = SKShapeNode()
-    
     //variables needed for flicking
     var start = CGPoint()
     var startTime = NSTimeInterval()
@@ -27,13 +26,17 @@ class GameScene: SKScene {
     let kMinSpeed = CGFloat(100)
     let kMaxSpeed = CGFloat(500)
     
-    var bgImage = SKSpriteNode(imageNamed: "flickitbg.png")
-    var startLabel = SKLabelNode(text: "START")
-    var launchTriangle = SKSpriteNode(imageNamed: "red_triangle.png")
-    var rulesLabel = SKLabelNode(text: "RULES")
-    var titleLabel = SKLabelNode(text: "FLICK IT")
-    var curveUp = SKShapeNode()
-    var curveDown = SKShapeNode()
+    //variables that construct the Home Game Scene
+    let bgImage = SKSpriteNode(imageNamed: "flickitbg.png")
+    let startLabel = SKLabelNode(text: "START")
+    let rulesLabel = SKLabelNode(text: "RULES")
+    let titleLabel = SKLabelNode(text: "FLICK IT")
+    let curveUp = SKShapeNode()
+    let curveDown = SKShapeNode()
+    let triangle = SKShapeNode()
+    let aboutButton = GGButton(defaultButtonText: "ABOUT", buttonAction: goToAbout)
+    var curveUpAction = SKAction()
+    var curveDownAction = SKAction()
     
     override func didMoveToView(view: SKView) {
         createHomeScreen()
@@ -49,19 +52,24 @@ class GameScene: SKScene {
         // Create and add title to home screen
         setupTitleLabel()
         
+        //curve up shape
+        let radius = self.size.height/6
+        addCurvedLines(curveUp, dub1: 1.5*M_PI, dub2: M_PI, bol: false, arch: Double(self.size.height/2 + radius), radi: radius)
+        //curve down shape
+        addCurvedLines(curveDown, dub1: M_PI/2, dub2: M_PI, bol: true, arch: Double(self.size.height/2 - radius), radi: radius)
+        
         // Sets bg image to fill the entire screen
         bgImage.size = CGSize(width: self.size.width, height: self.size.height);
         bgImage.position = CGPointMake(self.size.width/2, self.size.height/2);
         
         // Set up start Label
-        setupStartLabel()
+        setupStartLabel(radius)
         
         // Set up rules Label
-        setupRulesLabel()
+        setupRulesLabel(radius)
         
-        //Add Curved Lines
-        addCurvedLines(curveUp, dub1: 1.5*M_PI, dub2: M_PI, bol: false)
-        addCurvedLines(curveDown, dub1: M_PI/2, dub2: M_PI, bol: true)
+        // Set up about Button
+        aboutButton.position = CGPointMake(self.frame.width * 8/10, self.frame.height/12)
         
         
         //set Z-positions
@@ -69,9 +77,11 @@ class GameScene: SKScene {
         startLabel.zPosition = 2
         rulesLabel.zPosition = 2
         titleLabel.zPosition = 2
+        aboutButton.zPosition = 2
         curveDown.zPosition = 3
         curveUp.zPosition = 3
         triangle.zPosition = 4
+        
         
         // Add all the elements to the screen
         self.addChild(bgImage)
@@ -81,20 +91,28 @@ class GameScene: SKScene {
         self.addChild(titleLabel)
         self.addChild(rulesLabel)
         self.addChild(startLabel)
+        self.addChild(aboutButton)
 
         // Have triangle first shoot up quickly, then slower and slower.
+        /*
         triangle.runAction(SKAction.sequence([
-            SKAction.moveBy(CGVectorMake(0, self.frame.height / 15), duration: 1),
-            SKAction.moveBy(CGVectorMake(0, self.frame.height / 15), duration: 15),
-            SKAction.moveBy(CGVectorMake(0, self.frame.height / 20), duration: 50)
+            SKAction.moveBy(CGVectorMake(0, self.size.height / 15), duration: 1),
+            SKAction.moveBy(CGVectorMake(0, self.size.height / 15), duration: 15),
+            SKAction.moveBy(CGVectorMake(0, self.size.height / 20), duration: 50)
             ])
         )
+        */
     }
     
-    func addCurvedLines(curve: SKShapeNode, dub1: Double, dub2: Double, bol: Bool) {
-        let circlePath : UIBezierPath = UIBezierPath(arcCenter: CGPointMake(CGFloat(self.frame.width/2), CGFloat(self.frame.height/4)), radius: CGFloat(self.frame.width/3), startAngle: CGFloat(dub1), endAngle: CGFloat(dub2), clockwise: bol)
+    func addCurvedLines(curve: SKShapeNode, dub1: Double, dub2: Double, bol: Bool, arch: Double, radi: CGFloat) {
+        let circlePath : UIBezierPath = UIBezierPath(arcCenter: CGPointMake(CGFloat(self.size.width/2), CGFloat(arch)), radius: radi, startAngle: CGFloat(dub1), endAngle: CGFloat(dub2), clockwise: bol)
+        if curve == curveUp {
+            curveUpAction = SKAction.followPath(circlePath.CGPath, asOffset: true, orientToPath: true, duration: 1)
+        }
+        else if curve == curveDown {
+            curveDownAction = SKAction.followPath(circlePath.CGPath, asOffset: true, orientToPath: true, duration: 1)
+        }
         curve.path = circlePath.CGPath
-        curve.position = CGPointMake(0, self.frame.height/2)
         curve.strokeColor = UIColor.grayColor()
         curve.lineWidth = 3
     }
@@ -111,44 +129,46 @@ class GameScene: SKScene {
     }
     
     func createTriangle() {
-        let rect: CGRect = CGRectMake(0, 0, self.size.width/5, self.size.width/5)
+        let rect: CGRect = CGRectMake(0, 0, self.size.width/6, self.size.width/6)
         triangle.path = self.triangleInRect(rect)
         triangle.strokeColor = UIColor(red: 160/255, green: 80/255, blue: 76/255, alpha: 1)
         triangle.fillColor = UIColor(red: 160/255, green: 80/255, blue: 76/255, alpha: 1)
-        triangle.position = CGPointMake(self.size.width/2, self.size.height/2);
+        triangle.position = CGPointMake(self.size.width/2 - triangle.frame.width/2, self.size.height/2);
         // Set names for the launcher so that we can check what node is touched in the touchesEnded method
         triangle.name = "launch triangle";
+        //could randomize rotation here
+        triangle.runAction(SKAction.rotateByAngle(CGFloat(M_PI/2), duration: 3))
     }
     
     func setupTrianglePhysics() {
-        triangle.userInteractionEnabled=false
-        triangle.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: launchTriangle.size.width, height: launchTriangle.size.height))
+        triangle.userInteractionEnabled = false
+        triangle.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: triangle.frame.width, height: triangle.frame.height))
         triangle.physicsBody?.dynamic = true
         triangle.physicsBody?.affectedByGravity=false
     }
     
     func setupTitleLabel() {
-        titleLabel.position = CGPointMake(self.frame.width/2, self.frame.height * 9 / 10);
+        titleLabel.position = CGPointMake(self.size.width/2 + self.size.width/5, self.size.height/2 - titleLabel.frame.height/2);
         titleLabel.horizontalAlignmentMode = .Center
         titleLabel.fontColor = UIColor.whiteColor()
         titleLabel.fontName = "Futura"
         titleLabel.fontSize = 45
     }
     
-    func setupStartLabel() {
-        startLabel.position = CGPointMake(self.frame.width/2, self.frame.height * 9 / 10);
+    func setupStartLabel(rad: CGFloat) {
+        startLabel.position = CGPointMake(self.size.width/2 - rad, self.size.height/2 + rad);
         startLabel.horizontalAlignmentMode = .Center
         startLabel.fontColor = UIColor.whiteColor()
         startLabel.fontName = "Futura"
-        startLabel.fontSize = 40
+        startLabel.fontSize = 30
     }
     
-    func setupRulesLabel() {
-        rulesLabel.position = CGPointMake(self.frame.width/2, self.frame.height / 10);
+    func setupRulesLabel(rad: CGFloat) {
+        rulesLabel.position = CGPointMake(self.size.width/2 - rad, self.size.height/2 - (rad + rulesLabel.frame.height));
         rulesLabel.horizontalAlignmentMode = .Center
         rulesLabel.fontColor = UIColor.whiteColor()
         rulesLabel.fontName = "Futura"
-        rulesLabel.fontSize = 40
+        rulesLabel.fontSize = 30
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -158,7 +178,6 @@ class GameScene: SKScene {
         // Save start location and time
         start = location
         startTime = touch.timestamp
-        
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -168,6 +187,13 @@ class GameScene: SKScene {
         var dx: CGFloat = location.x - start.x
         var dy: CGFloat = location.y - start.y
         let magnitude: CGFloat = sqrt(dx * dx + dy * dy)
+        
+        //doesn't recognize About Button location! need to fix!
+        if aboutButton.containsPoint(location) {
+            print("here")
+            aboutButton.action()
+        }
+        
         if (magnitude >= self.kMinDistance){
             // Determine time difference from start of the gesture
             dx = dx / magnitude
@@ -175,18 +201,28 @@ class GameScene: SKScene {
             let touchedNode=self.nodeAtPoint(start)
             //make it move
             touchedNode.physicsBody?.velocity=CGVectorMake(0.0, 0.0)
+            let radiu = self.size.height/6
             if (touchedNode.name == "launch triangle" && dy > 0){
-                touchedNode.physicsBody?.applyImpulse(CGVectorMake(0, 100*dy))
+                //move on the curve up path
+                
+                let circle = UIBezierPath(arcCenter: CGPointMake(0, radiu), radius: radiu, startAngle: CGFloat(1.5*M_PI), endAngle: CGFloat(M_PI), clockwise: false)
+                let test = SKAction.followPath(circle.CGPath, asOffset: true, orientToPath: true, duration: 3)
+                triangle.runAction(test)
+                //touchedNode.physicsBody?.applyImpulse(CGVectorMake(0, 100*dy))
             }
             if (touchedNode.name == "launch triangle" && dy < 0){
-                touchedNode.physicsBody?.applyImpulse(CGVectorMake(0, 100*dy))
+                //move on the curve down path
+                let circle = UIBezierPath(arcCenter: CGPointMake(0, -1*radiu), radius: radiu, startAngle: CGFloat(M_PI/2), endAngle: CGFloat(M_PI), clockwise: true)
+                let test = SKAction.followPath(circle.CGPath, asOffset: true, orientToPath: true, duration: 3)
+                triangle.runAction(test)
+                //triangle.runAction(curveDownAction)
+                //touchedNode.physicsBody?.applyImpulse(CGVectorMake(0, 100*dy))
             }
             //touchedNode.physicsBody?.applyImpulse(CGVectorMake(100*dx, 100*dy))
         }
     }
     
     func startGame() {
-        //titleLabel.removeFromSuperview()
         let scene: SKScene = StartGameScene(size: self.size)
         
         // Configure the view.
@@ -204,26 +240,25 @@ class GameScene: SKScene {
     }
    
     override func update(currentTime: CFTimeInterval) {
-//        /* Called before each frame is rendered */
-//        if (launchTriangle.position.y >= startSquare.position.y){
-//            // call method to start game
-//            // for now just remove all the elements to show something has happened
-//            self.removeAllChildren();
-//            self.startGame();
-//            launchTriangle.position.y = 0
-//        }
-//        
-//        if (launchTriangle.position.y <= rulesCircle.position.y){
-//            // call method to start game
-//            // for now just remove all the elements to show something has happened
-//            self.removeAllChildren();
-//            launchTriangle.position.y = 0
-//        }
+        /* Called before each frame is rendered */
+        if (triangle.position.y >= startLabel.position.y - 20){
+            // call method to start game
+            // for now just remove all the elements to show something has happened
+            self.removeAllChildren();
+            self.startGame();
+            triangle.position.y = 0
+        }
+        
+        if (triangle.position.y <= rulesLabel.position.y + rulesLabel.frame.height + 20){
+            // call method to show Rules
+            // for now just remove all the elements to show something has happened
+            self.removeAllChildren();
+            triangle.position.y = 0
+        }
     }
     
     func delay(delay:Double, closure:()->()) {
         dispatch_after(
             dispatch_time( DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
-        
     }
 }
