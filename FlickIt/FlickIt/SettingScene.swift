cbe13e95bed2bd11c902d4f81cf8c2162fa71a37
Abhishek Mangla <abhishekm@abhishekmmaclap.local>
2016-05-28 13:52:05 -0500

    updated settings page

--- FlickIt/FlickIt/SettingScene.swift
+++ FlickIt/FlickIt/SettingScene.swift
@@ -13,6 +13,10 @@ class SettingScene: SKScene {
     let backButton = SKLabelNode(text: "BACK")
     let highScoreButton = SKLabelNode(text: "See My HighScore")
     let shareButton = SKLabelNode(text: "Share with Friends")
+    let storeButton = SKLabelNode(text: "Items Inventory")
+    let inventoryButton = SKLabelNode(text: "Your Inventory")
+    var highScoreSubview = UIView()
+    var highScoreScene = SKScene()
     var originalScene = SKScene()
     
     func returnMain()
@@ -49,6 +53,12 @@ class SettingScene: SKScene {
         else if shareButton.containsPoint(location) {
             shareButton.fontColor = UIColor.grayColor()
         }
+        else if storeButton.containsPoint(location) {
+            storeButton.fontColor = UIColor.grayColor()
+        }
+        else if inventoryButton.containsPoint(location) {
+            inventoryButton.fontColor = UIColor.grayColor()
+        }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -57,26 +67,62 @@ class SettingScene: SKScene {
         let location: CGPoint = touch.locationInNode(self)
         // Save end location and time
         if backButton.containsPoint(location) {
-            backButton.fontColor = UIColor.whiteColor()
+            highScoreSubview.removeFromSuperview()
             returnMain()
             
         }
         else if highScoreButton.containsPoint(location) {
-            highScoreButton.fontColor = UIColor.whiteColor()
+            
             openHighScoreSubview()
         }
         else if shareButton.containsPoint(location) {
-            shareButton.fontColor = UIColor.whiteColor()
+            
             openShareSubview()
         }
+        else if storeButton.containsPoint(location) {
+            
+            openStoreSubview()
+        }
+        else if inventoryButton.containsPoint(location) {
+            
+            openInventorySubview()
+        }
+        backButton.fontColor = UIColor.whiteColor()
+        highScoreButton.fontColor = UIColor.whiteColor()
+        shareButton.fontColor = UIColor.whiteColor()
+        storeButton.fontColor = UIColor.whiteColor()
+        inventoryButton.fontColor = UIColor.whiteColor()
+//        if (!highScoreSubview.frame.containsPoint(location)) {
+//            
+//        }
     }
     
     func setOriginalScener(scene: SKScene) {
         originalScene = scene
     }
     
+    func openStoreSubview() {
+        print("show the store to purchase powerups")
+    }
+    
+    func openInventorySubview() {
+        print("show the user's inventory of powerups")
+    }
+    
     func openHighScoreSubview() {
         print("show cool high scores table with local, state, and national level maybe")
+        let prevHighScore: Int = NSUserDefaults.standardUserDefaults().integerForKey("score")
+        highScoreSubview = UIView(frame: CGRectMake(1/7*view!.frame.size.width, 1/5*view!.frame.size.height, 5/7*view!.frame.size.width, 3/5*view!.frame.size.height))
+        highScoreSubview.backgroundColor = UIColor.whiteColor()
+        
+        let it = NSArray(array: ["Local", "National", "World"])
+        let segmentedCont = UISegmentedControl(items: it as [AnyObject])
+        
+        segmentedCont.frame = CGRectMake(1/7*self.view!.frame.size.width, 1/5*view!.frame.size.height, 5/7*view!.frame.size.width, 1/5*view!.frame.size.height)
+        highScoreSubview.addSubview(segmentedCont)
+        
+        
+        self.view?.addSubview(highScoreSubview)
     }
     
     func openShareSubview() {
@@ -105,6 +151,20 @@ class SettingScene: SKScene {
         shareButton.fontName = "BigNoodleTitling"
         shareButton.fontSize = 50
         self.addChild(shareButton)
+        
+        storeButton.position = CGPointMake(self.frame.size.width/2, self.frame.size.height*4/10);
+        storeButton.horizontalAlignmentMode = .Center
+        storeButton.fontColor = UIColor.whiteColor()
+        storeButton.fontName = "BigNoodleTitling"
+        storeButton.fontSize = 50
+        self.addChild(storeButton)
+        
+        inventoryButton.position = CGPointMake(self.frame.size.width/2, self.frame.size.height*3/10);
+        inventoryButton.horizontalAlignmentMode = .Center
+        inventoryButton.fontColor = UIColor.whiteColor()
+        inventoryButton.fontName = "BigNoodleTitling"
+        inventoryButton.fontSize = 50
+        self.addChild(inventoryButton)
         
     }
     
